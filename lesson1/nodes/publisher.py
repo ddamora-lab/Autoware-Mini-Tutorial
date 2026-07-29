8a4a151a863324682e071595fae1dc71fef586fb
ddamora-lab <ddamora@unisa.it>
2026-07-29 12:16:24 +0300

    Resolution issue lesson 1

--- lesson1/nodes/publisher.py
+++ lesson1/nodes/publisher.py
@@ -5,14 +5,15 @@ from std_msgs.msg import String
 
 class Publisher:
     def __init__(self):
+        # Parameters
+        self.message = rospy.get_param('~message', 'Hello World!')
+        self.rate_value = rospy.get_param('~rate', 2.0)
+
         # Internal variables
-        self.rate = rospy.Rate(rospy.get_param('~rate', 2.0))
+        self.rate = rospy.Rate(self.rate_value)
 
         # Publishers
         self.pub = rospy.Publisher('/message', String, queue_size=10)
-
-        # Parameters
-        self.message = rospy.get_param('~message', 'Hello World!')
 
     def run(self):
         while not rospy.is_shutdown():
